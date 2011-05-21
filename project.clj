9c5f397f559435fee524aa6ca0eeac4c363dde3b
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-21 16:11:13 -0500

    Using enlive to make the markup for the homepage.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject seo-analyzer "1.0.0-SNAPSHOT"
   :description "The seo-analyzer grades a page's seo characteristics."
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [compojure "0.6.3"]]
+                 [compojure "0.6.3"]
+                 [enlive "1.0.0"]]
   :dev-dependencies [[appengine-magic "0.4.1"]]
   :aot [seo-analyzer.app-servlet])
--- src/seo_analyzer/core.clj
+++ src/seo_analyzer/core.clj
@@ -1,11 +1,19 @@
 (ns seo-analyzer.core
-  (:use compojure.core)
+  (:use [compojure.core]
+        [net.cgrand.enlive-html])
   (:require [appengine-magic.core :as ae]
             [compojure.route :as route]))
 
+(deftemplate home (ae/open-resource-stream "home.html") [title]
+             [:title] (content title)
+             [:h1] (content title))
+
+(defn homepage
+  [request]
+  (apply str (home "SEO Analyzer")))
 
 (defroutes main-routes
-  (GET "/" [] "<h1>SEO Analyzer</h1>")
+  (GET "/" [] homepage)
   (route/not-found "Page not found"))
 
 (def app-handler
