ac1488247f45b3f5c367cdd9570a86d9d75d0a06
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-21 14:30:26 -0500

    Switched the app to be a very simple compojure app.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject seo-analyzer "1.0.0-SNAPSHOT"
   :description "The seo-analyzer grades a page's seo characteristics."
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [compojure "0.6.3"]]
   :dev-dependencies [[appengine-magic "0.4.1"]])
--- src/seo_analyzer/core.clj
+++ src/seo_analyzer/core.clj
@@ -1,11 +1,15 @@
 (ns seo-analyzer.core
-  (:require [appengine-magic.core :as ae]))
+  (:use compojure.core)
+  (:require [appengine-magic.core :as ae]
+            [compojure.route :as route]
+            [compojure.handler :as handler]))
 
 
-(defn seo-analyzer-app-handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello, world!"})
+(defroutes main-routes
+  (GET "/" [] "<h1>SEO Analyzer</h1>")
+  (route/not-found "Page not found"))
 
+(def app-handler
+  (handler/site main-routes))
 
-(ae/def-appengine-app seo-analyzer-app #'seo-analyzer-app-handler)
+(ae/def-appengine-app seo-analyzer-app #'app-handler)
