73730b0877047141182adc3e67d9d9f14754a0f5
Alex Robbins <alexander.j.robbins@gmail.com>
2011-05-24 08:31:23 -0500

    Form is rendering, but the POST case isn't working right.

--- src/seo_analyzer/core.clj
+++ src/seo_analyzer/core.clj
@@ -1,23 +1,30 @@
 (ns seo-analyzer.core
   (:use [compojure.core]
-        [net.cgrand.enlive-html])
+        [net.cgrand.enlive-html]
+        [form-dot-clj.core]
+        [form-dot-clj.html-controls])
   (:require [appengine-magic.core :as ae]
-            [compojure.route :as route]
-            [form-dot-clj.core :as form]
-            [seo-analyzer.forms :only [show-analyze-form]]))
+            [compojure.route :as route]))
 
-(deftemplate home (ae/open-resource-stream "home.html") [title]
-             [:title] (content title)
-             [:h1] (content title)
-             [:#content] (content
-                           (show-analyze-form nil nil)))
+(def-field keyword-phrase)
+(def-field url [:url "Is that a valid url?"])
 
-(defn homepage
-  [request]
-  (apply str (home "SEO Analyzer")))
+(def-form analyze-form
+          {:size 60 :required "We need this to proceed"}
+          :Keyword-Phrase (textbox keyword-phrase)
+          :Url (textbox url))
+
+(deftemplate home (ae/open-resource-stream "home.html") [params errors]
+             [:form] (html-content
+                       (show-controls analyze-form params errors)
+                       (default-submit "Analyze")))
+
+(deftemplate results (ae/open-resource-stream "results.html") [])
 
 (defroutes main-routes
-  (GET "/" [] homepage)
+  (GET "/" [] (home {} {}))
+  (POST "/" {params :params}
+        (on-post analyze-form params results home))
   (route/not-found "Page not found"))
 
 (def app-handler
